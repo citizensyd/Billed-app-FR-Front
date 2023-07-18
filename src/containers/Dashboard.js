7f07769e96794a9bc71ec61e08cc3a28247bd939
citizensyd <59608029+citizensyd@users.noreply.github.com>
2023-07-18 15:30:40 +0200

    Fix - Admin - handleSubmitAdmin bad data-testid to log in

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,6 +131,7 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
+    console.log("handleShowTickets");
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
